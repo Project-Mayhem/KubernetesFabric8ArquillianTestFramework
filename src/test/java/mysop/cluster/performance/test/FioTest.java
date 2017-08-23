e38f69f056b730b4a1ab081ea31713945f2d2546
falenn <dev@imwiz.com>
2017-08-23 16:44:34 -0400

    Here's a skeleton of the first of many fio tests.  I still don't understand what is running within k8s and when.  This currently does not connect to k8s.

--- src/test/java/mysop/cluster/performance/test/FioTest.java
+++ src/test/java/mysop/cluster/performance/test/FioTest.java
@@ -1,5 +1,43 @@
 package mysop.cluster.performance.test;
 
-public class FioTest {
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.File;
+
+import org.jboss.arquillian.container.test.api.RunAsClient;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.fabric8.kubernetes.client.KubernetesClient;
+
+@RunWith(Arquillian.class)
+//@RunAsClient
+/**
+ * Assuming this is actually running from within a namespace in a pod in a k8s cluster
+ * @author curtisbates
+ *
+ */
+public class FioTest {
+	
+	public static final Long ONEHUNDERED_GB = new Long("1000000000");
+	
+	@ArquillianResource
+	KubernetesClient client;
+
+	/**
+	 * Checks to see if the expected persistent volume exists - the pod should have a
+	 * 100GB volume available at /data
+	 */
+	@Test
+	public void testPersistentVolumeExists() throws Exception{
+		System.out.println(client.getConfiguration());
+		Long totalSpace;	
+		File data = new File("/data");
+		if(!data.isDirectory())
+			throw new Exception("Location not a directory");
+		totalSpace = data.getTotalSpace();
+		assertThat(totalSpace).isEqualByComparingTo(ONEHUNDERED_GB);
+	}
 }
